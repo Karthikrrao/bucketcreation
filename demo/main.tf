8dfd4e28c9f57a684eb3a58c12bcd122732bbecf
Karthikrrao <50327987+Karthikrrao@users.noreply.github.com>
2021-08-24 09:45:49 +0530

    Update main.tf

--- demo/main.tf
+++ demo/main.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
 
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket1"
+  acl    = "private"
+
+ tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
