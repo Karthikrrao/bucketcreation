8b9964d961af59adb2fea02e0162f36b9501eb4f
Karthikrrao <50327987+Karthikrrao@users.noreply.github.com>
2021-08-24 10:17:59 +0530

    Update variables.tf

--- demo/variables.tf
+++ demo/variables.tf
@@ -3,3 +3,14 @@ variable "aws_s3_bucket" {
   type = string
   default = "my-tf-test-bucket-test"
 }
+variable "s3_bucket_name" {
+  description = "s3 bucket name"
+  type        = string
+  default     = "Karthik_s3"
+}
+
+variable "s3_env" {
+  description = "environment for s3"
+  type        = string
+  default     = "Karthik_DEV_s3"
+} 
