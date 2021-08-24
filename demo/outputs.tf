5ed9da92ae5f265f761c5a77d67bf3fab52f0be4
Karthikrrao <50327987+Karthikrrao@users.noreply.github.com>
2021-08-24 10:41:38 +0530

    Update outputs.tf

--- demo/outputs.tf
+++ demo/outputs.tf
@@ -1,15 +1,15 @@
 output "bucket_domain_name" {
-  value       = aws_s3_bucket.b.bucket_domain_name
+  value       = aws_s3_bucket.new_bucket.bucket_domain_name
   description = "FQDN of bucket"
 }
 
 output "bucket_id" {
-  value       = aws_s3_bucket.b.id
+  value       = aws_s3_bucket.new_bucket.id
   description = "Bucket Name (aka ID)"
 }
 
 output "bucket_region" {
-  value       = aws_s3_bucket.b.region
+  value       = aws_s3_bucket.new_bucket.region
   description = "Bucket region"
 } 
  
